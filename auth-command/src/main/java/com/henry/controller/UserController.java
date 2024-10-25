12b2c7dbd626f4efc06aee1a86ac41b526593ec9
Henry <henry.pham@alpaca.vn>
2024-10-25 10:03:35 +0700

    build: init project v1.3

--- auth-command/src/main/java/com/henry/controller/UserController.java
+++ auth-command/src/main/java/com/henry/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.henry.controller;
 
-import com.henry.base.exception.ServiceException;
 import com.henry.base.service.response.WrapResponse;
-import com.henry.constant.AuthErrorCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +13,6 @@ public class UserController {
 
     @GetMapping("/hello")
     public CompletableFuture<WrapResponse<String>> hello() {
-        throw new ServiceException(AuthErrorCode.TEST);
+        return CompletableFuture.supplyAsync(() -> WrapResponse.ok("OK"));
     }
 }
--- auth-shared/src/main/java/com/henry/constant/AuthErrorCode.java
+++ auth-shared/src/main/java/com/henry/constant/AuthErrorCode.java
@@ -1,5 +1,15 @@
 package com.henry.constant;
 
 public class AuthErrorCode {
+
+    //AUTH_SERVER
+    public static final String USER_NOT_FOUND = "AUTH.USER_NOT_FOUND";
+    public static final String USER_USERNAME_EMPTY = "AUTH.USER_USERNAME_EMPTY";
+    public static final String USER_EMAIL_EMPTY = "AUTH.USER_EMAIL_EMPTY";
+    public static final String USER_PHONE_NUMBER_EMPTY = "AUTH.USER_PHONE_NUMBER_EMPTY";
+
+    public static final String PASSWORD_EMPTY = "AUTH.PASSWORD_EMPTY";
+
+
     public static final String TEST = "AUTH.TEST";
 }
