2f1bf45256d5510564c95359b4ca1efe2584b515
Henry <henry.pham@alpaca.vn>
2025-02-04 00:40:10 +0700

    build: init project v3

--- auth-command/src/main/java/com/henry/AuthCommandApplication.java
+++ auth-command/src/main/java/com/henry/AuthCommandApplication.java
@@ -1,13 +1,20 @@
 package com.henry;
 
+import com.henry.config.GradlePropertiesLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication(scanBasePackages = "com.henry")
 @EnableFeignClients
 public class AuthCommandApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthCommandApplication.class, args);
+        SpringApplication application = new SpringApplication(AuthCommandApplication.class);
+        application.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            new GradlePropertiesLoader(environment).loadGradleProperties();
+        });
+        application.run(args);
     }
 }
--- auth-flow/src/main/java/com/henry/AuthFlowApplication.java
+++ auth-flow/src/main/java/com/henry/AuthFlowApplication.java
@@ -1,13 +1,20 @@
 package com.henry;
 
+import com.henry.config.GradlePropertiesLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.henry")
 public class AuthFlowApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AuthFlowApplication.class, args);
+        SpringApplication application = new SpringApplication(AuthFlowApplication.class);
+        application.addInitializers(applicationContext -> {
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            new GradlePropertiesLoader(environment).loadGradleProperties();
+        });
+        application.run(args);
     }
 }
